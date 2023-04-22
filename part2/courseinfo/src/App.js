a1733ee25137e0bb2662ecb088b2a20b306e2086
Yixiao Yuan <eshoyuan@gmail.com>
2023-04-22 20:56:40 +0100

    Complete part2a

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,79 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <b>total of {sum} exercises</b>
+
+const Part = ({ part }) =>
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+    <div>
+      {parts.map((part) => <div><Part part={part} /></div>)}
+    </div>
+
+
+const Course = ({ courses }) => {
+  return (
+    <div>
+    {courses.map(course =>
+      <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total sum={course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
+      </div>
+    )
+    }
+    </div>
+  )
+}
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return <Course courses={courses} />
+}
+
+export default App
